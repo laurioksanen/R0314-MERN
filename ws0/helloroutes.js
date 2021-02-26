913f1746794936c641be1b031e30a469d9ea8156
Lauri Oksanen <lauri.oksanen1@hotmail.com>
2021-02-26 17:47:28 +0200

    juuu

--- ws0/helloroutes.js
+++ ws0/helloroutes.js
@@ -1,12 +1,15 @@
 var http = require("http");
 http.createServer(function (request, response) {
+    console.log(request.url)
     if(request.url ==="/helloworld") {
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<table style=width:100% border=1px ><tr><th>Firstname</th><th>Lastname</th><th>Age</th></tr>");
         response.end("Hello Worlddddd");
+       
     } else {
         response.writeHead(200, { "Content-Type": "text/html" });
-        response.end("Hello Worlddddd");
+        response.end("Hello Worldddddhgggdgfgdg");
+       
     }
     console.log("m");
 
